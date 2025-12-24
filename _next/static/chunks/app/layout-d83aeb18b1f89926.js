ed57a174e1c109d471d4d16a8a5a15ff995a35bc
Orbi1025 <john941025@gmail.com>
2025-12-25 03:00:23 +0900

    i

--- _next/static/chunks/app/layout-d83aeb18b1f89926.js
+++ _next/static/chunks/app/layout-d83aeb18b1f89926.js
@@ -23,7 +23,7 @@
       s(9324);
       var l = s(2115);
       let o =
-        "https://app.uniswap.org/#/swap?inputCurrency=eth&outputCurrency=0xcomingsoon";
+        "https://app.uniswap.org/#/swap?inputCurrency=eth&outputCurrency=0x6927c51e1d97298c795ea79464e2ef32255737a9";
       function i() {
         let [e, t] = (0, l.useState)(!1);
         return (0, r.jsxs)("div", {
@@ -211,7 +211,7 @@
                         }),
                       }),
                       (0, r.jsx)("a", {
-                        href: "https://www.dextools.io/app/en/ether/pair-explorer/0xcomingsoon",
+                        href: "https://www.dextools.io/app/en/ether/pair-explorer/0x6927c51e1d97298c795ea79464e2ef32255737a9",
                         target: "_blank",
                         rel: "noopener noreferrer",
                         children: (0, r.jsx)("img", {
